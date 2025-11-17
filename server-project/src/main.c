412c9cfb884dfcbaff2139f2b4ff480d4c3c1129
Flavio De Musso <Demusso1617@gmail.com>
2025-11-17 10:24:08 +0100

    Implement get_temperature

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -23,14 +23,37 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "protocol.h"
 
 #define NO_ERROR 0
+#define SEED_RNG_ONCE() \
+	do { \
+		static int seeded = 0; \
+		if (!seeded) { \
+			srand((unsigned int)time(NULL)); \
+			seeded = 1; \
+		} \
+	} while (0)
 
 void clearwinsock() {
 #if defined WIN32
 	WSACleanup();
 #endif
+}
+
+void error_handler(const char *message) {
+	perror(message);
+	clearwinsock();
+	exit(EXIT_FAILURE);
+}
+
+float get_temperature(void) {
+	const float min = -10.0f;
+	const float max = 40.0f;
+	SEED_RNG_ONCE();
+	float normalized = (float)rand() / (float)RAND_MAX;
+	return min + normalized * (max - min);
 }
 
 int main(int argc, char *argv[]) {
@@ -52,9 +75,7 @@ int main(int argc, char *argv[]) {
 	// Create TCP socket
 	my_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (my_socket < 0) {
-		perror("socket() failed");
-		clearwinsock();
-		return EXIT_FAILURE;
+		error_handler("socket() failed");
 	}
 
 	// TODO: Configure server address
