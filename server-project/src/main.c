9951d9dc10f4b26bf705b54cada64d025c6bca0b
Flavio De Musso <Demusso1617@gmail.com>
2025-11-17 10:26:09 +0100

    Implement build_server_address

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -151,6 +151,15 @@ int parse_arguments(int argc, char *argv[], unsigned short *port) {
 	return 0;
 }
 
+struct sockaddr_in build_server_address(unsigned short port) {
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	return server_addr;
+}
+
 int main(int argc, char *argv[]) {
 
 	// TODO: Implement server logic
