7ab9637c9c9449427142bd1542e9cab03f4c1346
Flavio De Musso <Demusso1617@gmail.com>
2025-11-17 10:24:38 +0100

    Implement get_wind

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -64,6 +64,14 @@ float get_humidity(void) {
 	return min + normalized * (max - min);
 }
 
+float get_wind(void) {
+	const float min = 0.0f;
+	const float max = 100.0f;
+	SEED_RNG_ONCE();
+	float normalized = (float)rand() / (float)RAND_MAX;
+	return min + normalized * (max - min);
+}
+
 int main(int argc, char *argv[]) {
 
 	// TODO: Implement server logic
